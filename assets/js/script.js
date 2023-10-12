f5cb1a7181edca63664922f17b971388997a8ae1
Osorkon21 <Osorkon21@users.noreply.github.com>
2023-10-12 00:14:52 -0500

    Add comments, clean up JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,51 +1,46 @@
-// ## Acceptance Criteria
-
-// WHEN I click the save button for that timeblock
-// THEN the text for that event is saved in local storage
-// WHEN I refresh the page
-// THEN the saved events persist
-
-// $(function () {
-// TODO: Add a listener for click events on the save button. This code should
-// use the id in the containing time-block as a key to save the user input in
-// local storage. HINT: What does `this` reference in the click listener
-// function? How can DOM traversal be used to get the "hour-x" id of the
-// time-block containing the button that was clicked? How might the id be
-// useful when saving the description in local storage?
-//
-// TODO: Add code to get any user input that was saved in localStorage and set
-// the values of the corresponding textarea elements. HINT: How can the id
-// attribute of each time-block be used to do this?
-// });
-
 const currentDay = $("#currentDay");
 const timeBlocksDiv = $(".time-blocks");
 
-var saveBtn;
+// dayjs object with current date/time
 var now = dayjs();
 
 start();
 
+// adds elements to page
 function start() {
   setDate();
   renderTimeBlocks();
 }
 
+// adds current date to top of screen
 function setDate() {
 
   // // "Do" displays today's date with ordinal - requires "advancedFormat" plugin to work
   currentDay.text(now.format("dddd, MMMM Do"));
 }
 
+// adds time block elements, adds localStorage contents if they exist
 function renderTimeBlocks() {
+
+  // today's date with time set to 9 AM
   var todayAtNine = dayjs(now.format("MM/DD/YYYY ") + "09:00:00");
 
+  // adds time block elements spanning 9 AM to 5 PM
   for (var i = 0; i < 8; i++) {
+
+    // time block's time
     var blockTime = todayAtNine.add(i, "hour");
+
+    // difference between now and the time block's time, in hours
     var timeDiff = blockTime.diff(now, "hour");
+
+    // id of time block
     var id = "hour-" + blockTime.format("H");
+
+    // localStorage string, if it exists
     var textAreaVal = JSON.parse(localStorage.getItem(id)) || "";
 
+    // raw HTML to append, populated with necessary variables
     timeBlocksDiv.append($(`
       <div id="${id}" class="row time-block ${getTimePeriod(timeDiff)}">
         <div class="col-2 col-md-1 hour text-center py-3">${blockTime.format("h A")}</div>
@@ -58,6 +53,7 @@ function renderTimeBlocks() {
   }
 }
 
+// adds past (grey), present (red), and future (green) colors to time blocks
 function getTimePeriod(timeDiff) {
   if (timeDiff > 0)
     return "future";
@@ -67,13 +63,20 @@ function getTimePeriod(timeDiff) {
     return "present";
 }
 
+// save button click event handler
 function onSaveBtnClick() {
+
+  // get id of time block
   var timeBlockID = $(this).parent().attr("id");
+
+  // get string from text area
   var textAreaVal = $(this).siblings(".description").val();
 
+  // if text area is not empty, save contents to localStorage
   if (textAreaVal !== "") {
     localStorage.setItem(timeBlockID, JSON.stringify(textAreaVal));
   }
 }
 
+// time blocks event listener with event delegation
 timeBlocksDiv.on("click", ".saveBtn", onSaveBtnClick);
